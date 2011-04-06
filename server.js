ec1790ab12ae67c886e20ee8bbd7b6040c22ccc6
Mike Larsson <mikelarssonftw@gmail.com>
2011-04-05 22:13:56 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+var HOST = "rubato.no.de";
+var PORT = 80;
+
+var http = require('http');
+
+function Server(delegate) {
+  var self = {};
+
+  var server = http.createServer(function(req, res) {
+    delegate(req, res);
+  });
+
+  self.setDelegate = function(aDelegate) {
+    delegate = aDelegate;
+  };
+  
+  server.listen(PORT, HOST); /* this is slow, we only want to do it once for the test run */
+
+  return self;
+}
+
+function RequestHandler() {
+  return function handler(req, res) {
+    res.writeHead('404 Not Found', {'Content-Type': 'text/plain'});
+    res.end('NO U\n');
+  };
+};
+
+var no_u = Server();
+no_u.setDelegate(RequestHandler());
