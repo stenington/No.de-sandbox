14b356c65b472bd5526e63acc268049a74cb52f8
Mike Larsson <mikelarssonftw@gmail.com>
2011-06-19 01:22:09 -0400

    Fixed more server.js typos and errors

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ var PORT = 80;
 var dispatch = require('root-handler');
 var rig = require('server-rig');
 
-var rig = rig.createServer(PORT, HOST);
-shell.delegateTo(dispatch.create());
+var rig = rig.createDelegatingServer(PORT, HOST);
+rig.delegateTo(dispatch.create());
